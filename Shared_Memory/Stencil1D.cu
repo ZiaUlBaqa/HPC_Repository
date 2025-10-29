5fac4dc03ae220548d8deaba0f98e0a4fb89c754
Zia <ziaulbaqa5@gmail.com>
2025-10-29 23:37:59 +0500

    1D stencil happening

--- Shared_Memory/Stencil1D.cu
+++ Shared_Memory/Stencil1D.cu
@@ -2,32 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
+//constants
 #define arraysize 1000000
+#define BlockDim 1024
+#define Halo 2
+
+
+//This is example of stencil in 1D, ehhehehehehhe???
+
+
 
 __global__ void kernel1(int * array)
 {
-    __shared__ int s_array[1024];  //Notice overhere we are only allocaating the memory in shared memory needed by the block and not the entire array for each block, hehehe boiiiii
+    __shared__ int s_array[BlockDim + Halo];
     int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 
     if(thread_id < arraysize)
     {
-        s_array[threadIdx.x] = array[thread_id];
-        __syncthreads();
+        s_array[threadIdx.x+1] = array[thread_id];
 
-        s_array[threadIdx.x] += 1;
+        if(threadIdx.x == 0)
+        {
+            s_array[threadIdx.x] = s_array[threadIdx.x+1];
+        }
+
+        if(threadIdx.x == blockDim.x-1)
+        {
+            s_array[threadIdx.x+2] = s_array[threadIdx.x+1];
+        }
+        __syncthreads();
 
         //copying it back
 
-        array[thread_id] = s_array[threadIdx.x];
+        array[thread_id] = (s_array[threadIdx.x] + s_array[threadIdx.x+1] + s_array[threadIdx.x+2])/3 ;
+
     }
 }
 
 __global__ void kernel2(int * array)
-{
-
-    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+{   int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
     if(thread_id < arraysize)
-        array[thread_id] +=1;
+    {
+        array[thread_id] = (array[thread_id+1]+array[thread_id]+array[thread_id-1])/3;
+    }
 }
 
 int main()
